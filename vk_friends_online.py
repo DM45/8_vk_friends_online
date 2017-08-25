ea187b620bd9f425663e68bfbf4e92cace3c1527
DM45 <mda.tomsk@gmail.com>
2017-08-25 19:25:56 +0700

    Исправление ошибок
    Запрос друзей онлайн происходит сразу

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,9 +1,18 @@
 import vk
-import json
 import getpass
 
 
 APP_ID = 6160966
+
+
+def get_user_login():
+    login = input('Enter login: ')
+    return login
+
+
+def get_user_password():
+    password = getpass.getpass('Enter password: ')
+    return password
 
 
 def get_online_friends(login, password):
@@ -11,24 +20,23 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
     api = vk.API(session)
-    friends_online = [
-        friends for friends in api.friends.get(fields='online') 
-        if friends.get("online") == 1
-    ]
+    friends_online_id = api.friends.getOnline()
+    friends_online = api.users.get(user_ids=friends_online_id)
     return friends_online
 
 
 def output_friends_to_console(friends_online):
     print('Friends online: ')
     for friends in friends_online:
-        print(friends.get("last_name"), friends.get("first_name"))
+        print(friends['first_name'], friends['last_name'])
 
 
 if __name__ == '__main__':
-    login = input('Enter login: ')
-    password = getpass.getpass('Enter password:')
+    login = get_user_login()
+    password = get_user_password()
     try:
         _friends_online = get_online_friends(login, password)
         output_friends_to_console(_friends_online)
