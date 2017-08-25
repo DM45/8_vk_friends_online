31d825b4689db18832c828a51eb2c679b4da7acb
DM45 <mda.tomsk@gmail.com>
2017-08-25 19:37:58 +0700

    Исправление ошибки
    Дополнено условие на except

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -40,5 +40,5 @@ if __name__ == '__main__':
     try:
         _friends_online = get_online_friends(login, password)
         output_friends_to_console(_friends_online)
-    except:
+    except vk.exceptions.VkAuthError:
         print("Wrong login or password")
